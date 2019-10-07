41aa1706a4aa24ac80b924cdf1b760b25110b219
Connor Bell <connor.n.bell@gmail.com>
2019-10-07 10:34:09 -0400

    Initial Commit

---
+++ Show Filter Sandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Show Filter Sandbox
+//
+//  Created by Connor Bell on 2019-10-07.
+//  Copyright © 2019 Mav Farm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
